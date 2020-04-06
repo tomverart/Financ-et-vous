7fbe2c7725fd8f46a4bcc0061a984c4baee8b2dc
Audran COLLIGNON <audran.collignon@gmail.com>
2020-04-06 14:14:57 +0200

    Scripts création tables et fonctions de base

---
+++ backend/model/etatNote.model.js
@@ -0,0 +1,44 @@
+const database = require('./database');
+
+class ETATNOTE {
+  static toSqltable () {
+    return `
+        CREATE TABLE ${ETATNOTE.tableName} (
+            -- idEtatNote, stringEtatNote
+            idEtatNote SERIAL PRIMARY KEY,
+            stringEtatNote VARCHAR
+        )
+    `;
+  }
+
+  static async createEtatNotes (newEtatNote) {
+    const result = await database.client.query({
+      text: `
+            INSERT INTO ${ETATNOTE.tableName} (stringEtatNote) VALUES ($1)`,
+      values: [newEtatNote]
+    });
+    console.log(result);
+  }
+
+  static async selectAllEtatNotes () {
+    const result = await database.client.query({
+      text: `
+            SELECT * FROM ${ETATNOTE.tableName}`
+    });
+    console.log(result);
+  }
+
+  static async selectByIdEtatNotes (idEtatNote) {
+    const result = await database.client.query({
+      text: `
+            SLECT * FROM ${ETATNOTE.tableName} where idEtatNote = ($1)`,
+      values: [idEtatNote]
+    });
+    console.log(result);
+  }
+}
+
+/** @type {String} **/
+ETATNOTE.tableName = 'ETATNOTE';
+
+module.exports = ETATNOTE;
---
+++ backend/model/noteFrais.model.js
@@ -0,0 +1,57 @@
+const database = require('./database');
+const UTILISATEUR = require('./utilisateur.model.js');
+const ETATNOTE = require('./etatNote.model.js');
+
+class NOTEFRAIS {
+  static toSqltable () {
+    return `
+        CREATE TABLE ${NOTEFRAIS.tableName} (
+            -- idNoteFrais, #idUtilisateur, #idEtatNote
+
+            idNoteFrais SERIAL PRIMARY KEY,
+            idUtilisateur INTEGER REFERENCES ${UTILISATEUR.tableName}(idUtilisateur),
+            idEtatNote INTEGER REFERENCES ${ETATNOTE.tableName}(idEtatNote)
+        )
+    `;
+  }
+
+  static async createNoteFrais (newNoteFrais) {
+    const result = await database.client.query({
+      text: `
+            INSERT INTO ${NOTEFRAIS.tableName} (stringNoteFrais) VALUES ($1)`,
+      values: [newNoteFrais]
+    });
+    console.log(result);
+  }
+
+  static async deleteNoteFrais (idNoteFrais) {
+    const result = await database.client.query({
+      text: `
+            DELETE FROM ${NOTEFRAIS.tableName} where idNoteFrais = ($1)`,
+      values: [idNoteFrais]
+    });
+    console.log(result);
+  }
+
+  static async selectAllNoteFrais () {
+    const result = await database.client.query({
+      text: `
+            SELECT * FROM ${NOTEFRAIS.tableName}`
+    });
+    console.log(result);
+  }
+
+  static async selectByIdNoteFrais (idNoteFrais) {
+    const result = await database.client.query({
+      text: `
+            SELECT * FROM ${NOTEFRAIS.tableName} where idNoteFrais = ($1)`,
+      values: [idNoteFrais]
+    });
+    console.log(result);
+  }
+}
+
+/** @type {String} **/
+NOTEFRAIS.tableName = 'NOTEFRAIS';
+
+module.exports = NOTEFRAIS;
