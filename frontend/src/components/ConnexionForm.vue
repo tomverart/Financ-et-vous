c3318d9ab375fa14be257746f0279b1b4d4cc1df
Tom <austin@intechinfo.fr>
2020-04-09 11:40:09 +0200

    interface de connexion initiale

---
+++ frontend/src/components/ConnexionForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <label for="login">login</label>
+      <input v-model="login" type="text" id="login"/>
+
+      <label for="password">password</label>
+      <input v-model="password" type="text" id="password">
+
+      <button v-on:click="sendData">envoyer donnée</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConnexionForm',
+  data() {
+      return {
+        login: null,
+        password: null
+      }
+  },
+  props: {
+  },
+  methods: {
+      sendData(){
+          this.$emit('log',
+           { Login: this.login,
+             Password: this.password
+           });
+           this.login = null;
+           this.password = null; 
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
