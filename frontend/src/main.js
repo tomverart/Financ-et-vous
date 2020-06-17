ff9a337f35a3b12c3e5693ba948fc5cc47da88f0
Tom <austin@intechinfo.fr>
2020-06-17 09:48:48 +0200

    modif

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,9 +13,9 @@ import EmployeeDashboard from './components/EmployeeDashboard.vue';
 import noteFraisComptable from './components/NoteFraisComptable.vue';
 import createExpenseReport from './components/CreateExpenseReport.vue';
 import createExpense from './components/CreateExpense.vue';
-import VueAxios from 'vue-axios'
+//import VueAxios from 'vue-axios'
 import axios from 'axios';
-import { BootstrapVue } from 'bootstrap-vue';
+//import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 axios.defaults.withCredentials = true;
