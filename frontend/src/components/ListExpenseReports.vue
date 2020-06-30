671fb9b593347e7e1c78fdf7073221e906bec39a
Audran COLLIGNON <audran.collignon@gmail.com>
2020-06-30 18:16:34 +0200

    fix €

--- frontend/src/components/ListExpenseReports.vue
+++ frontend/src/components/ListExpenseReports.vue
@@ -57,7 +57,7 @@
               <b-col sm="3" class="text-sm-right">
                 <b>Montant:</b>
               </b-col>
-              <b-col>{{ row.item.montant }} €</b-col>
+              <b-col>{{ row.item.montant }}</b-col>
             </b-row>
 
             <b-row class="mb-2">
